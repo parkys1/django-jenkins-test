da72fb4b14955173178c9809dbc6c713990f46d2
parkys1 <parkys1@parkys1-VirtualBox.(none)>
2014-01-05 23:43:27 +0900

    add File

--- storage/models.py
+++ storage/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+
+class File(models.Model):
+    name = models.CharField(null=False, max_length=4096)
+    size = models.IntegerField(null=False)
